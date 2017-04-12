c0c71fcec15a68036b2855173f1593225ef25bf1
mateo Sierra <mateo Sierra>
2017-04-11 23:51:23 -0500

    Creacion de dropdowns de bachillerato y universidad en header, y creacion de thumnails de libros en pagina principal con botones de comprar y detalles

--- view/mainPage.php
+++ view/mainPage.php
@@ -54,27 +54,82 @@
 
     <div class="container marketing">
 
-      <!-- Three columns of text below the carousel -->
       <div class="row">
-        <div class="col-lg-4">
-          <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-      </div><!-- /.row -->
+
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="thumbnail ">
+            <img class="imgMain" src="assets/images/imagesMain/pressman.jpg" alt="not found ">
+            <div class="caption"><br>
+              <h3>Ing. de Software</h3>
+              <p>Pressman</p>
+              <p><a href="#" class="btn btn-primary" role="button">Comprar</a> <a href="#" class="btn btn-default" role="button">Detalles</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="thumbnail ">
+            <img class="imgMain" src="assets/images/imagesMain/calculo.jpg" alt="not found ">
+            <div class="caption"><br>
+              <h3>Calculo diferencial</h3>
+              <p>Alberto Camacho</p>
+              <p><a href="#" class="btn btn-primary" role="button">Comprar</a> <a href="#" class="btn btn-default" role="button">Detalles</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="thumbnail ">
+            <img class="imgMain" src="assets/images/imagesMain/investiguemos.jpg" alt="not found ">
+            <div class="caption"><br>
+              <h3>Investiguemos 9</h3>
+              <p>Ciencias Naturales 9°</p>
+              <p><a href="#" class="btn btn-primary" role="button">Comprar</a> <a href="#" class="btn btn-default" role="button">Detalles</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+        <br><br><br>
+
+      <!-- Siguiente fila -->
+
+      <div class="row">
+
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="thumbnail ">
+            <img class="imgMain" src="assets/images/imagesMain/baldor.jpg" alt="not found ">
+            <div class="caption"><br>
+              <h3>Algebra de Baldor</h3>
+              <p>Aurelio Baldor</p>
+              <p><a href="#" class="btn btn-primary" role="button">Comprar</a> <a href="#" class="btn btn-default" role="button">Detalles</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="thumbnail ">
+            <img class="imgMain" src="assets/images/imagesMain/ingles.jpg" alt="not found ">
+            <div class="caption"><br>
+              <h3>World English 2</h3>
+              <p>National Geographic - Heinle</p>
+              <p><a href="#" class="btn btn-primary" role="button">Comprar</a> <a href="#" class="btn btn-default" role="button">Detalles</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="thumbnail ">
+            <img class="imgMain" src="assets/images/imagesMain/guerra.jpg" alt="not found ">
+            <div class="caption"><br>
+              <h3>La segunda guerra mundial</h3>
+              <p>Liddell Hart</p>
+              <p><a href="#" class="btn btn-primary" role="button">Comprar</a> <a href="#" class="btn btn-default" role="button">Detalles</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
 
 
       <!-- START THE FEATURETTES -->
